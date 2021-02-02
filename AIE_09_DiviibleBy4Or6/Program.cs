e515a7f3496349ec0a41bb6d55b8d5eed4071a07
Sam Yorng <HOMESUPYO2.0@gmail.com>
2021-02-02 12:45:15 -0800

    Class Exercises

--- AIE_09_DiviibleBy4Or6/Program.cs
+++ AIE_09_DiviibleBy4Or6/Program.cs
@@ -6,7 +6,19 @@ namespace AIE_09_DiviibleBy4Or6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            int sum = 4;
+
+            if ((sum % 6 == 0) || (sum % 6 == 0))
+            {
+                Console.WriteLine("Divisble by 4 Or 6");
+            }
+
+
+
+            else
+            {
+                Console.WriteLine("Not Divisable by 6");
+            }
+     }
+        
 }
