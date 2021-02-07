5ff0c509683885d9536d8555d4c54a4802dd8451
Samnang Yorng <s210537@students.aie.edu.au>
2021-02-08 10:32:08 +1100

    fixed merge errors

--- AIE_09_DiviibleBy4Or6/Program.cs
+++ AIE_09_DiviibleBy4Or6/Program.cs
@@ -8,7 +8,6 @@ namespace AIE_09_DiviibleBy4Or6
         {
             int sum = 4;
 
-<<<<<<< HEAD
             if (sum % 4 == 0)
             {
                 Console.WriteLine("This number is divisible by 4");
@@ -20,19 +19,4 @@ namespace AIE_09_DiviibleBy4Or6
             }
         }
     }
-=======
-            if ((sum % 6 == 0) || (sum % 6 == 0))
-            {
-                Console.WriteLine("Divisble by 4 Or 6");
-            }
-
-
-
-            else
-            {
-                Console.WriteLine("Not Divisable by 6");
-            }
-     }
-        
->>>>>>> e515a7f3496349ec0a41bb6d55b8d5eed4071a07
 }
