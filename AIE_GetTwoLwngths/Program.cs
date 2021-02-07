d8b901a35764a0de7f65fdfb37d88d7081e90e81
Samnang Yorng <s210537@students.aie.edu.au>
2021-02-08 09:51:43 +1100

    New

---
+++ AIE_GetTwoLwngths/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AIE_GetTwoLwngths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myInts = new int[10] { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+            for (int i = 0; i < 10; i++)
+            {
+                // we can access the value at a specified location in the array
+                // we use the [] subscript operator
+
+                Console.WriteLine(myInts[i]);
+            }
+
+        }
+    }
+}
